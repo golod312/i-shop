b604a2fd27cb47d63cd3ed359da4e64716100953
Andrii <83414932+golod312@users.noreply.github.com>
2022-05-18 17:40:44 +0300

    Updates

---
+++ precache-manifest.cbce83130778c51fcedf2bb6a4082d8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c546878b1cdfb4d50a1f0e05951ea5c",
+    "url": "/i-shop/index.html"
+  },
+  {
+    "revision": "8828471e385c3be50fa0",
+    "url": "/i-shop/static/css/2.8bd440b1.chunk.css"
+  },
+  {
+    "revision": "36baa53f0846df3b5eb4",
+    "url": "/i-shop/static/css/main.ab74e8ca.chunk.css"
+  },
+  {
+    "revision": "8828471e385c3be50fa0",
+    "url": "/i-shop/static/js/2.9b054881.chunk.js"
+  },
+  {
+    "revision": "36baa53f0846df3b5eb4",
+    "url": "/i-shop/static/js/main.8feda375.chunk.js"
+  },
+  {
+    "revision": "3db29ea2be1f791586c6",
+    "url": "/i-shop/static/js/runtime~main.21f950c3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/i-shop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/i-shop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/i-shop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/i-shop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/i-shop/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
